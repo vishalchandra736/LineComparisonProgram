b36ad04702d2abcf9401eb74d26d095c369e20c6
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 23:41:15 +0530

    Check the Equality of Two Lines

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -3,10 +3,9 @@ package com.blz.linecomparisonproblem;
 import java.util.Scanner;
 
 public class LineComparisonProblem {
-    public static void main(String[] args) {
+    public static int lineCoordinate() {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Every Line consisting 2 points and each point consisting 2 co-ordinates (x,y).");
         System.out.print("Enter the First point - X co-ordinate : ");
         int x1 = scan.nextInt();
         System.out.print("Enter the First point - Y co-ordinate : ");
@@ -16,6 +15,18 @@ public class LineComparisonProblem {
         System.out.print("Enter the Second point - Y co-ordinate : ");
         int y2 = scan.nextInt();
         int Length_Of_Line = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("Length of a Line is : " + Length_Of_Line);
+        return Length_Of_Line;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Every Line consisting 2 points and each point consisting 2 co-ordinates (x,y).");
+        System.out.println("For Line 1 - ");
+        int Length_of_line1 = lineCoordinate();
+        System.out.println("For Line 2 - ");
+        int Length_of_line2 = lineCoordinate();
+        if (Length_of_line1 == Length_of_line2)
+            System.out.println("Both Lines are Equal");
+        else
+            System.out.println("Lines are not Equal");
     }
 }
