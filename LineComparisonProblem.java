d805f45a8c60aaf8405db108a412e8685502850e
vishalchandra736 <vishalchandra736@gmail.com>
2022-12-23 19:40:54 +0530

    Welcome to Line Comparison Computation Program

---
+++ LineComparisonProblem.java
@@ -0,0 +1,7 @@
+package com.blz.linecomparisonproblem;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
